11b033ddce53b4d3d1f387eb93002782187f1f07
Mori <112848048+nek0logical@users.noreply.github.com>
2022-09-08 18:01:58 +0800

    Update flip.py

--- flip.py
+++ flip.py
@@ -4,7 +4,7 @@ async def flip_coin(request):
   result = random.randint(0,1)
   if result == 0:
     output = "heads"
-   elif result == 1:
+  elif result == 1:
     output = "tails"
     
-   return f"You flipped a coin! It landed {output} side up"
+  return f"You flipped a coin! It landed {output} side up"
